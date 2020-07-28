ec088ceae1de68b087775de54057892171a4358f
thewesleywong <60673366+thewesleywong@users.noreply.github.com>
2020-07-28 01:55:18 -0700

    Add files via upload
    update with new ports

--- servo_test.ino
+++ servo_test.ino
@@ -3,27 +3,35 @@
  *  by Wesley Wong
  */
 
-//ultrasonic sensor
-#include <HCSR04.h>
-// Initialize sensor that uses digital pins 12 and 11.
-int triggerPin = 12;
-int echoPin = 11;
-UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
-//Test distance = (high level time × velocity of sound (340m/s)/2
-
 // Servo Setup
 #include <Servo.h>
 Servo myservo;  // create servo object to control a servo
 int servingsFirst = 2;  // number of servings for first dispenser
 Servo myservo2; // second servo
-int servingsSecond = 1; // number of servings for second dispenser
+int servingsSecond = 2; // number of servings for second dispenser
+
+// conveyor dc motor1 setup
+int speedPin1 = 13;
+int dir1a = 12;
+int dir1b = 11;
+
+// conveyor dc motor2 setup
+int speedPin2 = 10;
+int dir2a = 9;
+int dir2b = 8;
+
+int mSpeed = 255; // max speed
+int start = 0; // startup counter variable
+int conveyorSpeed = 125; // speed at which the tray will move
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(7,INPUT);   // photosensor module initialization port
-  pinMode(6,INPUT);  // second photosensor initialization
-  myservo.attach(9);   //  attaches the servo on pin 9 to the servo object
-  myservo2.attach(10); // second servo to pin 10
+  pinMode(3,INPUT);   // photosensor module initialization port
+  pinMode(4,INPUT);  // second photosensor initialization
+  myservo.attach(6);   //  attaches the servo on pin 9 to the servo object
+  myservo2.attach(7); // second servo to pin 10
+  //pinMode(speedPin1, OUTPUT); // set pin 11 to output to conveyor motor 1 enable
+  pinMode(speedPin2, OUTPUT); // set pin 12 to output to conveyor motor 2 enable
   Serial.begin(9600);  // initialization of serial port
   Serial.println("Ready to connect/nDefault password is 1234 or 000");
   delay(1000);
@@ -36,9 +44,9 @@ void loop() {
 
   // for loop to create sum variables for the sensor values to smooth out the readings and prevent false triggers
   for (int i = 0; i < 10; i++) {
-    if(digitalRead(7) == HIGH)// assign light sensor one to port 52
+    if(digitalRead(5) == HIGH)// assign light sensor one to port 52
       valOne++; // increment val one if sensor covered
-    if(digitalRead(6) == HIGH)      // light sensor two to port 50
+    if(digitalRead(4) == HIGH)      // light sensor two to port 50
       valTwo++; // increment val two if second sensor covered
     delay(10);
   }
@@ -76,14 +84,19 @@ void loop() {
   {
     // dispenses first module
     Serial.println("both");
+    myservo.write(100); // reverses directon for a bit to unclog
+    myservo2.write(100); // reverses directon for a bit to unclog
+    delay(20);
     myservo.write(90 - (45 * servingsFirst)); // dispense out
     myservo2.write(90 - (45 * servingsFirst)); // dispense out second servo
-    //myservo.write(100); // reverses directon for a bit to unclog
+    delay(100);
   }
   else if(tempOne == true and tempTwo == false)               //first blocked, second unblocked
   {
     // dispenses first module
     Serial.println("one");
+    myservo.write(100); // reverses directon for a bit to unclog
+    delay(20);
     myservo.write(90 - (45 * servingsFirst)); // dispense out
     myservo2.write(88.9); // stops second servo
     //myservo.write(100); // reverses directon for a bit to unclog
@@ -92,6 +105,8 @@ void loop() {
   {
     // dispenses first module
     Serial.println("two");
+    myservo2.write(100); // reverses directon for a bit to unclog
+    delay(20);
     myservo.write(88.9); // stops first servo
     myservo2.write(90 - (45 * servingsFirst)); // dispense out second
     //myservo.write(100); // reverses directon for a bit to unclog
@@ -104,5 +119,29 @@ void loop() {
     myservo2.write(88.9); // stops first servo
     //myservo.write(100); // reverses directon for a bit to unclog
   }
+  // direction of motors for conveyor
+  digitalWrite(dir1a, HIGH);
+  digitalWrite(dir1b, LOW);
+  
+  digitalWrite(dir2a, HIGH);
+  digitalWrite(dir2b, LOW);
 
+  // this function kickstarts the dc motors
+  if (start == 0)
+  {
+    analogWrite(speedPin2, mSpeed); // start motor 2
+    analogWrite(speedPin1, mSpeed); // start motor 1
+    delay(100);
+    start++;
+  }
+
+  // conveyor on
+  analogWrite(speedPin1, mSpeed);
+  analogWrite(speedPin2, mSpeed);
+  Serial.println("motor");
+  delay(80);
+  analogWrite(speedPin1, 0);
+  analogWrite(speedPin2, 0);
+  delay(80);
+  Serial.println("test");
 }
